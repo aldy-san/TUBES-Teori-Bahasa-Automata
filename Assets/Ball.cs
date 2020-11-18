9b7b366b0e7517bb37693779ff70562d59699e1b
aldy-san <nsmaulidi12@gmail.com>
2020-11-18 12:47:52 +0700

    Selesai
    +Scene Main Menu
    +Scene GamePlay
    +Scene About
    +Scene Kalah
    +Scene Menang

---
+++ Assets/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Ball : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "cekKena")
+        {
+            Debug.Log("Kena Kau");
+            Cursor.visible = true;
+            SceneManager.LoadScene(2);
+        }
+        if (other.gameObject.tag == "Menang")
+        {
+            Debug.Log("Menang kau");
+            Cursor.visible = true;
+            SceneManager.LoadScene(3);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("COLIIISINI");
+    }
+}
---
+++ Assets/PointBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointBall : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 target;
+    void Start()
+    {
+        Cursor.visible = false;
+        ball.transform.position = new Vector2(0, 0);
+    }
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        ball.transform.position = new Vector2(target.x,target.y);
+    }
+}
